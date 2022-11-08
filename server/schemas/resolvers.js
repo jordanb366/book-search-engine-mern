73d4439c6f812a9c3eb32db8f079ecaf0c5d93d0
Jordan <jordan.b3360@gmail.com>
2022-11-08 16:00:09 -0600

    small changes to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
     // get a single user by either their id or their username
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("books");
+        return User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError("You need to be logged in!");
     },
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
-    bookCount: INT
+    bookCount: Int
     savedBooks: [Book]!
   }
 
